a1fd95f91cd0b4f3317c40f93cf3414e81cd6507
AMLAN1000 <153717048+AMLAN1000@users.noreply.github.com>
2026-01-12 20:52:09 +0600

    Fix Vercel deployment configuration

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -2,9 +2,9 @@ import jwt from "jsonwebtoken";
 import config from "../config";
 
 export const generateToken = (payload: object): string => {
-  return jwt.sign(payload, config.jwt.secret! as string, {
+  return jwt.sign(payload, config.jwt.secret!, {
     expiresIn: config.jwt.expires_in!,
-  });
+  } as jwt.SignOptions);
 };
 
 export const verifyToken = (token: string): any => {
